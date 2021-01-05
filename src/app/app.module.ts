da23957ae99e385781377e623e4a0d11426a118c
Luis Fernando Nestares Valverde <68039881+luisfernando1901@users.noreply.github.com>
2021-01-04 23:28:04 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//Importamos el módulo de ReactiveFormsModule
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormComponent } from './components/form/form.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatChipsModule} from '@angular/material/chips';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatChipsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+//Importamos el FormBuilder y el FormGroup
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+  //Inicializamos el form principal de tipo FormGroup
+  formPrincipal:FormGroup;
+
+  constructor(private fb:FormBuilder) { }
+  //Dentro del init creamos los campos del form
+  ngOnInit(): void {
+    this.formPrincipal = this.fb.group({
+      name:[''],
+      age:[''],
+      email:[''],
+      company:[''],
+
+    });
+
+  }
+
+}
